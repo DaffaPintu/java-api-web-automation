ca91c14511cac4d8a3a9cef65a63fa16e87329a2
DaffaPintu <daffa@pintu.co.id>
2023-09-29 01:18:34 +0700

    first commit

---
+++ src/test/java/helper/Endpoint.java
@@ -0,0 +1,9 @@
+package helper;
+
+public class Endpoint {
+    public static final String host = "https://reqres.in/";
+    public static final String GET_LIST_USER = host + "api/users";
+
+    public static final String host_gorest = "https://gorest.co.in/public/v2/";
+    public static final String USER_GOREST = host_gorest + "users";
+}
